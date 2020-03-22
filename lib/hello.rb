da550d28680df3fc08d3d84bd42a04e88a4dc842
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-22 01:15:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ def hello_t(array)
     yield array[i] # i represents the index position here and           #will yield whatever is in that index position
     i = i + 1
   end
+  array
 end
 
 # call your method here!
